e8d886f8c4d9461d08758e5f9f8840759e5e967c
solovyshn@gmail.com <solovyshn@gmail.com>
2023-03-24 14:29:56 +0100

    Removing items

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { TodoItem } from '../interfaces/todo-item';
 
 @Component({
@@ -6,13 +6,23 @@ import { TodoItem } from '../interfaces/todo-item';
   template: `
     <div class="todo-item">
       {{ item.title }}
+
+      <button class="btn btn-red"
+          (click)="removeItem()">
+          remove
+      </button>
     </div>
   `,
   styleUrls: ['./todo-item.component.scss']
 })
 export class TodoItemComponent implements OnInit {
   @Input() item: TodoItem;
+  @Output() remove: EventEmitter<TodoItem> = new EventEmitter<TodoItem>();
+
   constructor(){}
   ngOnInit(): void {
+  }
+  removeItem(): void {
+    this.remove.emit(this.item);
   }
 }

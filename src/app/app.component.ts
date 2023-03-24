90052b2e1426b735365e414a693388bfda392de6
solovyshn@gmail.com <solovyshn@gmail.com>
2023-03-24 13:31:04 +0100

    Your Message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import { Component } from '@angular/core';
 
     <ul>
       <li *ngFor="let todoItem of todoList">
-        {{ todoItem.title }}
+        <app-todo-item [item]="todoItem"></app-todo-item>
       </li>
     </ul>
   `,
